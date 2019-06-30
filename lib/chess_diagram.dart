1ecf8a125081fd208ab31fc80eb5ef6d2d846fe4
Laurent Bernabé <laurent.bernabe@gmail.com>
2019-06-30 18:55:03 +0200

    Starting to draw chess diagram: cells and coordinates.

---
+++ lib/chess_diagram.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class ChessDiagramCell extends StatelessWidget {
+  final Color whiteColor = Color.fromARGB(255, 255, 206, 158);
+  final Color blackColor = Color.fromARGB(255, 209, 139, 71);
+  final bool cellColor;
+
+  ChessDiagramCell([this.cellColor]);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () => {},
+      child: null,
+      color: this.cellColor ? whiteColor : blackColor,
+    );
+  }
+}
+
+class ChessDiagram extends StatefulWidget {
+  @override
+  _ChessDiagramState createState() => _ChessDiagramState();
+}
+
+class _ChessDiagramState extends State<ChessDiagram> {
+  _buildCells() {
+    var cellsList = <Widget>[];
+
+    // first files coordinates line
+    cellsList.add(Text(""));
+    for (var file = 0; file < 8; file++) {
+      final fileLetter = String.fromCharCode(file + 65);
+      cellsList.add(Center(
+        child: Text(
+          fileLetter,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ));
+    }
+    cellsList.add(Text(""));
+
+    // board with ranks coordinate
+    for (var row = 0; row < 8; row++) {
+      final rankDigit = String.fromCharCode(56 - row);
+      final rankText = Center(
+        child: Text(rankDigit,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            )),
+      );
+      cellsList.add(rankText);
+      for (var col = 0; col < 8; col++) {
+        cellsList.add(ChessDiagramCell((row + col) % 2 == 0));
+      }
+      cellsList.add(rankText);
+    }
+
+    // second files coordinates line
+    cellsList.add(Text(""));
+    for (var file = 0; file < 8; file++) {
+      final fileLetter = String.fromCharCode(file + 65);
+      cellsList.add(Center(
+        child: Text(
+          fileLetter,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ));
+    }
+    cellsList.add(Text(""));
+    return cellsList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = 200.0;
+    return SizedBox.fromSize(
+        size: Size.square(size),
+        child: Container(
+          child: GridView.count(
+            crossAxisCount: 10,
+            children: _buildCells(),
+          ),
+          color: Colors.grey,
+        ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
-// Flutter code sample for material.Scaffold.1
-
-// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
-// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
-// to center the text within the [Scaffold] and the [FloatingActionButton] is
-// centered and docked within the [BottomAppBar] using
-// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
-// connected to a callback that increments a counter.
-
 import 'package:flutter/material.dart';
+import 'chess_diagram.dart';
 
 void main() => runApp(MyApp());
 
@@ -40,7 +32,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         title: Text('Sample Code'),
       ),
       body: Center(
-        child: Text('You have pressed the button $_count times.'),
+        child: ChessDiagram(),
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
